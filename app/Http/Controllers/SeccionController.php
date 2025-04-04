6119b479e7d531c5a764a7ca83b6fc5fac4b6f3f
Alan Zuñiga <alan.zuniga@alumnos.udg.mx>
2025-04-03 18:02:15 -0600

    agregue reuqest update and store en SeccionController

--- app/Http/Controllers/SeccionController.php
+++ app/Http/Controllers/SeccionController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Seccion;
+use App\Http\Requests\StoreSeccionRequest;
+use App\Http\Requests\UpdateSeccionRequest;
 
-use Illuminate\Http\Request;
 
 class SeccionController extends Controller
 {
@@ -25,7 +27,7 @@ class SeccionController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreSeccionRequest $request)
     {
         //
     }
@@ -33,7 +35,7 @@ class SeccionController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Seccion $seccion)
     {
         //
     }
@@ -41,7 +43,7 @@ class SeccionController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Seccion $seccion)
     {
         //
     }
@@ -49,7 +51,7 @@ class SeccionController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateSeccionRequest $request, Seccion $seccion)
     {
         //
     }
@@ -57,7 +59,7 @@ class SeccionController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Seccion $seccion)
     {
         //
     }
