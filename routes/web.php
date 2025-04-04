7b65f777308aca20922af05a265364a0f1385961
Alan Zuñiga <alan.zuniga@alumnos.udg.mx>
2025-04-03 18:08:58 -0600

    rutas AlumnoController

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,29 @@
 <?php
 
-use App\Livewire\Settings\Appearance;
-use App\Livewire\Settings\Password;
-use App\Livewire\Settings\Profile;
-use App\Models\Docente;
-use App\Http\Controllers\DocenteController;
+use App\Http\Controllers\AlumnoController;
 use Illuminate\Support\Facades\Route;
-
+use Livewire\Volt\Volt;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::resource('docente', DocenteController::class);
-
-
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Route::post('alumno/{alumno}/actualizar-secciones', [AlumnoController::class, 'actualizarSeccionesAlumno'])
+    ->name('alumno.actualizar-secciones')
+    ->middleware(['auth']);
+
+Route::resource('alumno', AlumnoController::class)->middleware(['auth']);
+
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
-    Route::get('settings/profile', Profile::class)->name('settings.profile');
-    Route::get('settings/password', Password::class)->name('settings.password');
-    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
 
 require __DIR__.'/auth.php';
