29944775f8035eb7c29fcadfc5eccd063c8a3f48
Alan Zuniga <alan.zuniga@alumnos.udg.mx>
2025-04-07 15:36:35 -0700

    implmentacion de componenets

--- resources/views/alumnos/alumno-show.blade.php
+++ resources/views/alumnos/alumno-show.blade.php
@@ -1,7 +1,16 @@
 <x-layouts.app :title="__('Info Alumno')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            
+           
+        <x-alerta titulo="Primer alerta">
+        <ul>
+            <li> Element 1 </li>
+            <li> Element 2 </li>
+            <li> Element 3 </li>
+        </ul>
+        </x-alerta>
+        <x-alerta titulo="Segunda alerta"/>
+
             <h1>{{ $alumno->nombre }}</h1>
 
             <ul>
